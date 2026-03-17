91a6ff9c4a250608526bb15539d6c01b0edafb34
raescla <raescla.ribeiro@gmail.com>
2026-03-17 12:49:24 -0300

    Aula 15 - Uso do useState

--- src/app/index.jsx
+++ src/app/index.jsx
@@ -1,6 +1,46 @@
-import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native"
+import { useState } from 'react'; //Importar o uso de estado
+import { Image, StyleSheet, Text, TextInput, TouchableOpacity, View } from "react-native";
 
 export default function App() {
+    const [email, setEmail] = useState(''); // Declarar os estados
+    const [senha, setSenha] = useState('');
+    const [autenticado, setAutenticado] = useState(false);
+    const [tentativas, atualizarTentativas] = useState (0);
+
+    const bloqueado = tentativas >= 5;
+
+
+    // const admin = {
+    //     nome: "Raescla",
+    //     idade: 28,
+    //     gender: "Fem",
+    //     profissao: "Professora"
+    // }
+    
+    const url = autenticado === true
+    ? "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSymWJLDx6HxmQX66UW57SAJ4xPzt28s4RkKA&s"
+    : "https://images.icon-icons.com/2972/PNG/512/instagram_logo_icon_186894.png"
+
+    // let url = "https://images.icon-icons.com/2972/PNG/512/instagram_logo_icon_186894.png"
+
+    // if (autenticado === true){ // depende da variável de estado
+    //     url = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSymWJLDx6HxmQX66UW57SAJ4xPzt28s4RkKA&s"
+    // }
+
+    
+
+    function login(email,senha) {
+        atualizarTentativas(tentativas + 1);
+        // if(bloqueado) return null;
+
+        if(email === "raescla" && senha ==="12345"){
+            setAutenticado (true)
+        } else {
+            setAutenticado (false)
+        }
+        console.log(email === "raescla", senha ==="12345")
+    }
+
     return (
 
         <View style={styles.container}>
@@ -8,22 +48,59 @@ export default function App() {
                 width={100}
                 height={100}
                 source={{
-                    uri: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcSljqaDsIkzZQsQ01cpqsnbJ4TF75ZfXhVHQg&s"
+                    uri: url
                 }}
                 style={styles.logo}
             />
-            <TextInput 
-                placeholder="Email:" // poderia ter declarado apenas como styles.input, que serviria para ambos
-                style={styles.email} 
-            />
-            <TextInput placeholder="Senha:"
-                style={styles.senha} /> 
 
-            <TouchableOpacity>
+            {autenticado && (
+        <View style={{ backgroundColor: 'green', borderRadius: 4, paddingHorizontal: 24, paddingVertical: 8}}>
+          <Text style={{ color: "white" }}>Você está autenticado!</Text>
+        </View>
+      )}
+
+    
+    {bloqueado && (
+        <View style={{ backgroundColor: 'red', borderRadius: 4, paddingHorizontal: 30, paddingVertical: 10 }}>
+          <Text style={{ color: "white" }}>Você está bloqueado!</Text>
+        </View>
+      )}
+
+
+           
+
+        {!autenticado && (    
+            <TextInput 
+                style={styles.email}
+                placeholder="Email:" // poderia ter declarado apenas como styles.input, que serviria para ambos
+                value={email} // Aula 15
+                onChangeText={(textoDigitado) => setEmail(textoDigitado)} // Aula 15
+                
+            />
+        )}
+            
+
+        {!autenticado && (  
+            <TextInput 
+            placeholder="Senha:" //texto de fundo
+                style={styles.senha} 
+                value={senha}
+                onChangeText={(textoDigitado) => setSenha(textoDigitado)}
+                secureTextEntry
+                /> 
+        )}
+
+
+        {(!autenticado || !bloqueado) && (
+            <TouchableOpacity
+            style={styles.button}
+            onPress={() => login (email, senha)}
+            >
+                
                 <Text style={styles.entrar}>Entrar</Text>
                 
             </TouchableOpacity>
-
+        )}
         </View>
     )
 }
@@ -77,18 +154,42 @@ const styles = StyleSheet.create({
         marginLeft: 65,
         marginRight: 65,
         borderColor: "#818381",
-        backgroundColor: "#000000",
-        color: "#FFF",
-        
+        backgroundColor: "#3ba7ff",
+        color: "#ffffff",
 
     },
-
-
-
 })
 
 
 
+// Contador de Cliques
+// import {useState} from 'react';
+// import {View, Text, Button, StyleSheet} from 'react-native';
+
+// export default function Contador() {
+//     const [count, setCount] = useState(0);
+
+//     return (
+//         <View style={styles.container}>
+//             <Text style={styles.texto}>Cliques: {count}</Text>
+//             <Button
+//             title='Adicionar'
+//             onPress={() =>setCount(count + 1)}
+//             />
+//         </View>
+//     );
+// }
+
+// const styles = StyleSheet.create({
+//     container: {
+//         borderRadius: 50,
+//         padding: 35,
+//         flex: 1,
+//         justifyContent: "center"
+        
+//     },
+// }
+// )
 
 
 
@@ -101,70 +202,7 @@ const styles = StyleSheet.create({
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// flexDirection: "column",
-// alignItems: "center",
-// paddingVertical: 48,
-// paddingHorizontal: 32,
-// backgroundColor: "#fdfffe",
-// gap: 32
 
 
 // CTRL + J para abrir o terminal, e npm start para iniciar o APP
 
-// const styles = StyleSheet.create({
-//     container: {
-//     flex: 1,
-//     flexDirection: "column",
-//     alignItems: "center",
-//     paddingVertical: 48,
-//     paddingHorizontal: 32,
-//     backgroundColor: "#fdfffe",
-//     gap: 32
-//   },
-//   card: {
-//     width: "100%",
-//     borderRadius: 16,
-//     padding: 24,
-//     gap: 16,
-//     backgroundColor: "#ececec",
-//     alignItems: "center"
-//   },
-//    circle: {
-//     width: 100,
-//     height: 100,
-//     backgroundColor: "#151316",
-//     borderRadius: "100%"
-//   },
-
-//   titulo: {
-//     fontSize: 22,
-//     fontWeight: "700"
-//   },
-//   button: {
-//     width: "100%",
-//     paddingVertical: 16,
-//     backgroundColor: "#9900ff",
-//     borderRadius: 16,
-//     alignItems: "center"
-//   },
-//   text: {
-//     color: "#ebe3e3",
-//     fontWeight: "bold",
-//     fontSize: 16
-//   }
-// })
